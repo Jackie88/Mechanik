a5319da6d971f4fd18066437f5948f593daf0879
pomiot <pomiot@gmail.com>
2014-01-18 23:42:26 +0100

    Changelog: *Znajdowanie ruchu rozpoznaje i zwraca teraz także ilość pól, o które ma się łapka poruszyć (strasznie brzydko to wygląda, jest topornie i lepiej byłoby to przepisać jak będzie na to chwila - niemniej jednak działa)

--- Mechanik/src/CommandFinder/movementFinder.java
+++ Mechanik/src/CommandFinder/movementFinder.java
@@ -16,22 +16,22 @@ public class movementFinder {
 					
 					for(String upWordPattern : movementUpList)
 					if(s2.matches(upWordPattern)){movementDirection[0]=1;
-					//movementDirection[1]=valueFinder.findValue(inputExpression);
+					movementDirection[1]=valueFinder.findValue(inputExpression);
 					break;}
 					
 					for(String rightWordPattern : movementRightList)
 					if(s2.matches(rightWordPattern)){movementDirection[0]=2;
-					//movementDirection[1]=valueFinder.findValue(inputExpression);
+					movementDirection[1]=valueFinder.findValue(inputExpression);
 					break;}
 					
 					for(String downWordPattern : movementDownList)
 					if(s2.matches(downWordPattern)){movementDirection[0]=3;
-					//movementDirection[1]=valueFinder.findValue(inputExpression);
+					movementDirection[1]=valueFinder.findValue(inputExpression);
 					break;}
 					
 					for(String leftWordPattern : movementLeftList)
 					if(s2.matches(leftWordPattern)){movementDirection[0]=4;
-					//movementDirection[1]=valueFinder.findValue(inputExpression);
+					movementDirection[1]=valueFinder.findValue(inputExpression);
 					break;}
 					
 		}}
---
+++ Mechanik/src/CommandFinder/valueFinder.java
@@ -0,0 +1,43 @@
+package CommandFinder;
+
+public class valueFinder {
+	public static int findValue(String[] inputExpression){
+		
+		int foundValue = -1;
+		String[][] numbersWordList = new String[11][2];	
+		
+		numbersWordList[0][0]="0";
+		numbersWordList[0][1]="zero";
+		numbersWordList[1][0]="1";
+		numbersWordList[1][1]="jeden";
+		numbersWordList[2][0]="2";
+		numbersWordList[2][1]="dwa";
+		numbersWordList[3][0]="3";
+		numbersWordList[3][1]="trzy";
+		numbersWordList[4][0]="4";
+		numbersWordList[4][1]="cztery";
+		numbersWordList[5][0]="5";
+		numbersWordList[5][1]="pi[eê][æc]";
+		numbersWordList[6][0]="6";
+		numbersWordList[6][1]="sze[sœ][cæ]";
+		numbersWordList[7][0]="7";
+		numbersWordList[7][1]="siedem";
+		numbersWordList[8][0]="8";
+		numbersWordList[8][1]="osiem";
+		numbersWordList[9][0]="9";
+		numbersWordList[9][1]="dziewi[eê][cæ]";
+		numbersWordList[10][0]="10";
+		numbersWordList[10][1]="dziesi[eê][cæ]";
+		
+		
+		for (String s1 : inputExpression){
+			for(int i = 0; i<=10; i++){
+				for(String j : numbersWordList[i]){
+					if(s1.matches(j)) foundValue=i;
+				}
+			}
+		}
+		return foundValue;
+		
+	}
+}
