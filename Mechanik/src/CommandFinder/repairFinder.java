6b3391ff03557b041ea00e16460dc1e926a1f7ba
pomiot <pomiot@gmail.com>
2014-01-30 19:58:06 +0100

    Więcej odpowiedzi mechanika.

--- Mechanik/src/CommandFinder/repairFinder.java
+++ Mechanik/src/CommandFinder/repairFinder.java
@@ -4,7 +4,7 @@ public class repairFinder {
 	public static String[] findRepair(String[] inputExpression){
 		
 		String[] foundCommand = {"-1","-1"};
-		String[] repairWordList = {"napraw","nareperuj","zreperuj","za[l設ataj","zaklej","zaklajstruj","napompuj","pompuj","[l設ataj"};
+		String[] repairWordList = {"napraw","nareperuj","zreperuj","za[l設ataj","zaklej","zaklajstruj","napompuj","pompuj","[l設ataj","na[l設aduj","[l設aduj"};
 		
 		for(String s1 : inputExpression){
 			for(String repairWordPattern : repairWordList)
